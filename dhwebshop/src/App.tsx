57c06cbe1851993971e3f7684ad9d36506075174
Frederik <frede4947@gmail.com>
2022-01-04 13:25:36 +0100

    Test Api assigned

--- dhwebshop/src/App.tsx
+++ dhwebshop/src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
 import {Col, Container, Row } from 'react-bootstrap';
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-function App() {
-  return (
-    <>
-      <Container>
-        <Row>
-          <Col>
-            App 1
-            <button>Default</button>
-          </Col>
-          <Col>
-            App 2
-          </Col>
-          <Col>
-            App 3
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
 }
+
+// Api connection
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+const App = () => {
+  // Get api objects
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+    );
+  console.log(data);
+
+  return <div className='App'>
+    <Container>
+  <Row>
+    <Col>
+      Col 1 
+      <button>Default</button>
+    </Col>
+    <Col>
+      Col 2
+    </Col>
+    <Col>
+      Col 3
+    </Col>
+  </Row>
+</Container>
+  </div>;
+  
+}
+
 
 export default App;
